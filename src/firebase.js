716caecd90aa3cc59df2f54825f0818ad35816ee
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-03-03 11:50:42 -0600

    Reinitialized separate firbase site

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBVmGz5Rk_bKWkYKcBbQ0k1J_WyXJ3E7gI",
-    authDomain: "ourspace-75c0b.firebaseapp.com",
-    databaseURL: "https://ourspace-75c0b.firebaseio.com",
-    projectId: "ourspace-75c0b",
-    storageBucket: "ourspace-75c0b.appspot.com",
-    messagingSenderId: "1091422180950",
-    appId: "1:1091422180950:web:e5704d85af09c509500df7",
-    measurementId: "G-M6Q26EBTPF"
+  apiKey: "AIzaSyC4ApsiSt4jC4W13xuW3aV-HoMQq8GP_mM",
+  authDomain: "travis-ourspace.firebaseapp.com",
+  databaseURL: "https://travis-ourspace.firebaseio.com",
+  projectId: "travis-ourspace",
+  storageBucket: "travis-ourspace.appspot.com",
+  messagingSenderId: "265561486250",
+  appId: "1:265561486250:web:28e5cfe6841598c64831d3",
+  measurementId: "G-8X6T2J9DPR"
 };
 
   firebase.initializeApp(firebaseConfig);
