863d0f0cf0f52d41854aca47a1715b25891db7fe
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-03-07 00:17:51 -0600

    Removed failed jest implementation

--- src/tests/Listing.test.js
+++ src/tests/Listing.test.js
@@ -2,10 +2,6 @@ import { render, fireEvent } from '@testing-library/react';
 import ListingList from '../components/Listing.js'
 
 test('ListingList is rendered', () => {
-    const testFn = jest.fn();
-    const propItems = [];
-    const user = {a: 1};
-    const house = {a: 1};
 
     const { getByTestId } = render(
         <ListingList items={[1, 2, 3, 4, 5]}/>
